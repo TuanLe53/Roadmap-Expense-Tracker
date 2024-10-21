dc84a621872c7ae2872916175f3e651939845d19
Tuan Le <leanhtuan030523@gmail.com>
2024-10-21 20:38:22 +0700

    Update add command, append new row to csv file

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import typer
 from rich import print
+from csv import writer
 
 app = typer.Typer()
 
 @app.command()
 def add(description: str = "", amount: int = 0):
     if not description or amount == 0:
-        print(f"[bold red]Error:[/bold red] Please provide a description and amount.")
-        return
+        raise ValueError("[bold red]Error:[/bold red] Please provide a description and amount.")
     
-    print(description, amount)
+    with open("tracker.csv", "a") as f:
+        writer_obj = writer(f)
+        writer_obj.writerow([description, amount])
+        
+        f.close()
     
 if __name__ == "__main__":
     app() 
