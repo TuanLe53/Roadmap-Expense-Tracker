92cf4ed669a26eb73093c599f2480df1e056ac22
Tuan Le <leanhtuan030523@gmail.com>
2024-10-22 19:41:45 +0700

    Add id and date columns

--- utils.py
+++ utils.py
@@ -3,11 +3,13 @@ from rich.console import Console
 
 def show_table(data):
     table = Table(show_header=True)
+    table.add_column("ID")
     table.add_column("Description")
     table.add_column("Price")
+    table.add_column("Date")
     
     for row in data:
-        table.add_row(row[0], row[1])
+        table.add_row(row[0], row[1], row[2], row[3])
         
     console = Console()
     console.print(table)
