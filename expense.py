771845ed362ec484eb0ba2164c3857dbd65a297a
Tuan Le <leanhtuan030523@gmail.com>
2024-10-22 19:27:42 +0700

    Create get_total_expense function

--- expense.py
+++ expense.py
@@ -1,4 +1,4 @@
-from csv import writer
+from csv import writer, reader
 
 def add_expense(description: str, amount: int) -> None:
     with open("tracker.csv", "a") as f:
@@ -6,3 +6,16 @@ def add_expense(description: str, amount: int) -> None:
         writer_obj.writerow([description, amount])
         
         f.close()
+        
+def get_total_expense() -> int:
+    with open("tracker.csv", "r") as f:
+        csv_reader = reader(f)
+        
+        #Skip header
+        next(csv_reader, None)
+        
+        total = 0
+        for row in csv_reader:
+            total += int(row[1])
+            
+        return total
