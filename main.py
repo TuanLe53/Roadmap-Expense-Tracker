a798751454edfc82460bdff96afba83fd98af7f1
Tuan Le <leanhtuan030523@gmail.com>
2024-10-24 19:35:02 +0700

    Add support for returning total value for a specific month in summary command

--- main.py
+++ main.py
@@ -26,10 +26,21 @@ def list(month: int = 0):
     
         
 @app.command()
-def summary():
-    total = get_total_expense()
-    print(f"Total expenses: ${total}")
-
+def summary(month: int = 0):
+    if 1 <= month <= 12:
+        expenses = get_expense_of_month(month)
+        total = get_total_expense(expenses)
+        print(f"Total expenses: ${total}")
+    elif month == 0:
+        with open("tracker.csv", "r") as f:
+            expenses = csv.reader(f)
+            #Skip header
+            next(expenses, None)
+            total = get_total_expense(expenses)
+        print(f"Total expenses: ${total}")
+    else:
+        raise ValueError("[bold red]Error:[/bold red] Invalid input! The program only accepts month values from 1 to 12, or you can leave it blank to get a report for all 12 months.")
+ 
 @app.command()
 def add(description: str = "", amount: int = 0):
     if not description or amount == 0:
