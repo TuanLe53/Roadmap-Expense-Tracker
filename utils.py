f5718841e39fd09c1bc91deb9cdf6ad48f0d3ed5
Tuan Le <leanhtuan030523@gmail.com>
2024-10-26 19:13:45 +0700

    Create write_csv_file function

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from rich.table import Table
 from rich.console import Console
+from csv import writer
 
 def show_table(data):
     table = Table(show_header=True)
@@ -13,3 +14,11 @@ def show_table(data):
         
     console = Console()
     console.print(table)
+    
+def write_csv_file(file_name: str, columns: list[str], data: list[list[str]]):
+    with open(file_name, "w") as f:
+        writer_obj = writer(f, lineterminator="\n")
+        writer_obj.writerow(columns)
+        
+        for record in data:
+            writer_obj.writerow(record)
